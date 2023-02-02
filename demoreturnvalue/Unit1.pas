555cab7263a151a64112cbcf4365d695cfc024f7
Geoffrey Smith <fork@tysontechnology.com.au>
2023-02-02 15:54:07 +1100

    Added a 3 parameter Tuple

--- demoreturnvalue/Unit1.pas
+++ demoreturnvalue/Unit1.pas
@@ -23,7 +23,8 @@ type
   private
     { Private declarations }
   public
-    function Test():TTuple<boolean,string>;
+    function Test(): TTuple<boolean, string>;
+    function Test3(): TTuple<boolean, string, Integer>;
     { Public declarations }
   end;
 
@@ -38,9 +39,18 @@ procedure TForm1.Button1Click(Sender: TObject);
 var
   success : boolean;
   errorMessage : String;
+  num : Integer;
 begin
   Test().Returns(success, errorMessage);
   ShowMessage(BoolToStr(success, True) + ' ' + errorMessage);
+
+  Test3().Returns(success, errorMessage, num);
+  ShowMessage(BoolToStr(success, True) + ' ' + errorMessage  + ' ' + num.ToString);
+end;
+
+function TForm1.Test3: TTuple<boolean, string, Integer>;
+begin
+  Exit(Result.SendBack(true, 'It Works', 32432));
 end;
 
 { TForm1 }
@@ -49,5 +59,7 @@ function TForm1.Test: TTuple<boolean, string>;
 begin
   Exit(Result.SendBack(true, 'It Works'));
 end;
+
+
 
 end.
--- demoreturnvalue/uTuple.pas
+++ demoreturnvalue/uTuple.pas
@@ -3,6 +3,18 @@ unit uTuple;
 interface
 
 type
+  TTuple<T, R, S> = record
+  strict private
+    FA : T;
+    FB : R;
+    FC : S;
+  public
+    class function SendBack(a: T; b: R; c: S):TTuple<T, R, S>; static;
+    constructor Create(a: T; b: R; c: S);
+    procedure Returns(var left:T; var right:R; var third:S);
+  end;
+
+
   TTuple<T, R> = record
   strict private
     FA : T;
@@ -30,6 +42,27 @@ procedure TTuple<T, R>.Returns(var left: T; var right: R);
 begin
   left := FA;
   right := FB;
+end;
+
+{ TTuple<T, R, S> }
+
+constructor TTuple<T, R, S>.Create(a: T; b: R; c: S);
+begin
+  FA := a;
+  FB := b;
+  FC := c;
+end;
+
+procedure TTuple<T, R, S>.Returns(var left: T; var right: R; var third: S);
+begin
+  left := FA;
+  right := FB;
+  third := FC;
+end;
+
+class function TTuple<T, R, S>.SendBack(a: T; b: R; c: S): TTuple<T, R, S>;
+begin
+  Result := TTuple<T, R, S>.Create(a, b, c);
 end;
 
 end.
